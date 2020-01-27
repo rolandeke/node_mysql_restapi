b04cf359cbca360f62f273c038f7caf436fb82f6
rolandeke <rolandeke49@gmail.com>
2020-01-27 19:09:51 +0000

    Added route for getting all users from the database and for getting a single user by id and by getting user by name

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,33 +2,50 @@ const express = require("express");
 const usersdb = require("../db/users");
 const router = express.Router();
 
+//Route for getting all users from the database
 router.get("/", async (req, res, next) => {
   try {
-    let result = await usersdb.all();
-    res.json(result);
+    const result = await usersdb.all();
+    if (result) {
+      res.json(result);
+    } else {
+      res.json({ msg: "No data to be fetched" });
+    }
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
   }
 });
 
+//Route for getting a single user from the database
 router.get("/:id", async (req, res, next) => {
   try {
-    let result = await usersdb.one(req.params.id);
-    res.json(result[0]);
+    if (isNaN(req.params.id) || req.params.id <= 0 || req.params.id == "") {
+      res.json({ msg: "Invalid input!!! Try Again" });
+      return;
+    }
+    const result = await usersdb.one(parseInt(req.params.id));
+    if (result) {
+      res.json(result);
+    } else {
+      res.json({ msg: `${req.params.id}  cannot be found` });
+    }
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
   }
 });
 
+//Route for getting Users by name
 router.get("/name/:name", async (req, res, next) => {
   try {
     let result = await usersdb.onebyname(req.params.name);
     res.json(result);
-  }catch(e){
-      console.log(e)
-      res.sendStatus(500)
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
   }
 });
+
+//Exporting Route
 module.exports = router;
